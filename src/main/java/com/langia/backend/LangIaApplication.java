a41bc3b60ac29528496683a419f6857e49db44e0
Jose Fabio Pereira Junior <fabio2543@gmail.com>
2025-11-20 09:46:03 -0300

    Primeiro commit - Estrutura inicial do LangIA

---
+++ src/main/java/com/langia/backend/LangIaApplication.java
@@ -0,0 +1,13 @@
+package com.langia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/langia/backend/LangIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.langia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
